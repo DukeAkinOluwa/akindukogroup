67e6ed408901dcde6097c08fc03db4624911d99a
DukeAkinOluwa <akindukoakinoluwa@gmail.com>
2024-12-26 00:54:26 +0100

    Update: Makes the about page responsive for mmobile phones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,12 @@
+'use client'
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar(){
+
+    const [isSideNavOpen, setIsSideNavOpen] = useState(false);
+
     const handleScrollClick = (sectionId) => {
       // Optionally, use refs for direct access (explained later)
       // const sectionElement = sectionId === 'section-1' ? section1Ref.current : section2Ref.current;
@@ -14,13 +20,17 @@ export default function NavBar(){
         top: document.getElementById(sectionId).offsetTop,
       });
     };
+    function handleHamburgerClick() {
+        setIsSideNavOpen(!isSideNavOpen);
+    }
+    
     return(
         <div className="navbar">
             <div className="left">
                 <img src="./Images/Logo.png" alt="Akinduko Group Logo" />
             </div>
             <div className="right">
-                <nav>
+                <nav className="longNav">
                     <Link href={`/`} >Home</Link>
                     <Link href={`/About`} >About Us</Link>
                     <Link href={`/Contact`} >Contact</Link>
@@ -28,6 +38,16 @@ export default function NavBar(){
                     <a href={`/About`} >About Us</a>
                     <a href={`/Contact`} >Contact</a> */}
                 </nav>
+                <nav className="sideNav" style={{left: `${isSideNavOpen ? '0px' : '-100dvw'}`}}>
+                    <Link href={`/`} >Home</Link>
+                    <Link href={`/About`} >About Us</Link>
+                    <Link href={`/Contact`} >Contact</Link>
+                </nav>
+            </div>
+            <div className="hamburgerMenuIcon" onClick={handleHamburgerClick}>
+                <div className="lines"></div>
+                <div className="lines"></div>
+                <div className="lines"></div>
             </div>
         </div>
     )
